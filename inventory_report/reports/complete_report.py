ff0bb0d5a1d7145de970ea825f4fe23febcf7f3f
martins <gabrielamspinola@gmail.com>
2023-02-06 11:09:08 -0300

    func: CompleteReport generate implementation

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,2 +1,22 @@
-class CompleteReport:
-    pass
+from inventory_report.reports.simple_report import SimpleReport
+from collections import Counter
+
+
+class CompleteReport(SimpleReport):
+    @staticmethod
+    def generate(data: list):
+        list_of_companies_qtd = ""
+
+        simple_report = SimpleReport.generate(list)
+
+        counter = Counter(data["nome_da_empresa"] for item in data)
+
+        for item in counter:
+            x, y = item
+            list_of_companies_qtd = f"- {x}: {y}\n"
+
+        return (
+            simple_report
+            + "Produtos estocados por empresa:"
+            + list_of_companies_qtd
+        )
