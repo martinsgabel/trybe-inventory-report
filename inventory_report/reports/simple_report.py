b05b024eadd0c85dc7ad9f5d62d8c77497aaf08e
martins <gabrielamspinola@gmail.com>
2023-02-06 10:36:33 -0300

    func: simple_report implementation

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,2 +1,28 @@
+from datetime import date
+from collections import Counter
+
+
 class SimpleReport:
-    pass
+    @staticmethod
+    def generate(self, list_of_dict):
+        fab_date = [
+            list_of_dict["data_de_fabricacao"] for item in list_of_dict
+        ]
+
+        valid_date = [
+            list_of_dict["data_de_validade"] for item in list_of_dict
+            if list_of_dict["data_de_validade"] > str(date.today())
+        ]
+
+        companies = [
+            list_of_dict["nome_da_empresa"] for item in list_of_dict
+        ]
+
+        company_t = Counter(companies).most_common(1)
+        x, y = company_t[0]
+
+        return (
+            f"Data de fabricação mais antiga: {min(fab_date)}"
+            f"Data de validade mais próxima: {max(valid_date)}"
+            f"Empresa com mais produtos: {x}"
+        )
