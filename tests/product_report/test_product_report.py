09d7d70547483949a17483b0256b7f90e7872a4d
martins <gabrielamspinola@gmail.com>
2023-02-07 14:13:09 -0300

    test: product report implemented

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -5,7 +5,7 @@ from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    Product(
+    report_instance = Product(
         '0',
         'Tap Shoes',
         'IDONEOS Ltda',
@@ -15,10 +15,14 @@ def test_relatorio_produto():
         'instruções'
     )
 
-    assert (
+    report_instance = str(report_instance)
+
+    report = (
         "O produto Tap Shoes"
         + " fabricado em 01/01/2021"
         + " por IDONEOS Ltda com validade"
         + " até 01/01/2050"
         + " precisa ser armazenado instruções."
     )
+
+    assert report == report_instance
